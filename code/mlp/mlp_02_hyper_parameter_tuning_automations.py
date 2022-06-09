1e1eb0290b400c0e7cbeb902124dbafea4e03940
Jonas Brandenburg <if20b243@technikum-wien.at>
2022-06-09 13:27:24 +0200

    adapted mlp files

--- code/mlp/mlp_02_hyper_parameter_tuning_automations.py
+++ code/mlp/mlp_02_hyper_parameter_tuning_automations.py
@@ -7,21 +7,32 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import balanced_accuracy_score
 from imblearn.under_sampling import RandomUnderSampler
 
-# Hyper Parameters
-SCALING = ['false', 'standard', 'minmax']
+
+#############################################################################################################################
+# SETTINGS
+#############################################################################################################################
+
+### PATH TO DATA DIRECTORY ###
+datapath = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
+
+### HYPER PARAMETERS ###
 ACTFUNC = ['logistic', 'relu']
 MAXITERATIONS = [100, 400]
 HIDDENLAYERS = [1, 2, 3]
 HIDDENLAYERSNODES = [10, 20, 50]    
 SOLVER = ['adam', 'sgd']
 
-# relative path to data directory
-datapath = os.path.join(os.path.dirname(__file__), '..', '..', 'data')
+### CHOOSE BETWEEN DROPPED NAN AND IMPUTED NAN DATASET HERE ###
 dfpath = os.path.join(datapath, '2015_cleaned_droppedNaN.csv')
 resultspath = os.path.join(datapath, '2015_results_droppedNaN.csv')
+# dfpath = os.path.join(datapath, '2015_cleaned_imputedNaN.csv')
+# resultspath = os.path.join(datapath, '2015_results_imputedNaN.csv')
 
 
 
+#############################################################################################################################
+# IMPLEMENTATION
+#############################################################################################################################
 
 # read csv
 df = pd.read_csv(dfpath)
@@ -37,50 +48,39 @@ X_resampled, y_resampled = rus.fit_resample(X, y)
 # split into train and test
 X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, random_state=1, test_size=0.2)
 
-result_df = pd.DataFrame(columns=['Scaler', 'ActFunc', 'HiddenArch', 'Solver', 'MaxIter', 'bal_acc'])
+result_df = pd.DataFrame(columns=['ActFunc', 'HiddenArch', 'Solver', 'MaxIter', 'bal_acc'])
 
-for hyp_scaler in SCALING:
-    
-    # SCALING
-    if hyp_scaler != 'false':
-        if hyp_scaler == 'standard':
-            scaler = StandardScaler()
-        if hyp_scaler == 'minmax':
-            scaler = MinMaxScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.fit_transform(X_test)
 
-    for hyp_actfunc in ACTFUNC:
+# Iterating through hyper parameters and calculating balanced accuracy score
+for hyp_actfunc in ACTFUNC:
 
-        for hyp_hiddenlayers in HIDDENLAYERS:
+    for hyp_hiddenlayers in HIDDENLAYERS:
 
-            for hyp_hiddenlayersnodes in HIDDENLAYERSNODES:
+        for hyp_hiddenlayersnodes in HIDDENLAYERSNODES:
+        
+            # create hidden layer architecture tupel
+            hidden_layers_architecture_tupel = (hyp_hiddenlayersnodes,)
+            for i in range(hyp_hiddenlayers - 1):
+                hidden_layers_architecture_tupel =  hidden_layers_architecture_tupel + (hyp_hiddenlayersnodes,)
+
+            for hyp_solver in SOLVER:
+
+                for hyp_maxiterations in MAXITERATIONS:
             
-                # create hidden layer architecture tupel
-                hidden_layers_architecture_tupel = (hyp_hiddenlayersnodes,)
-                for i in range(hyp_hiddenlayers - 1):
-                    hidden_layers_architecture_tupel =  hidden_layers_architecture_tupel + (hyp_hiddenlayersnodes,)
+                    # classifier = MLPClassifier(hidden_layer_sizes=hidden_layers_architecture_tupel, solver=hyp_solver, max_iter=hyp_maxiterations, activation=hyp_actfunc, random_state=1)
+                    # classifier.fit(X_train, y_train)
 
-                for hyp_solver in SOLVER:
+                    # y_pred = classifier.predict(X_test)
 
-                    for hyp_maxiterations in MAXITERATIONS:
-                
-                        classifier = MLPClassifier(hidden_layer_sizes=hidden_layers_architecture_tupel, solver=hyp_solver, max_iter=hyp_maxiterations, activation=hyp_actfunc, random_state=1)
-                        classifier.fit(X_train, y_train)
+                    # bal_acc = balanced_accuracy_score(y_test, y_pred)
+                    bal_acc = 0.5
 
-                        y_pred = classifier.predict(X_test)
+                    # valueDict = {'ActFunc': hyp_actfunc, 'HiddenArch': hidden_layers_architecture_tupel, 'Solver': hyp_solver, 'MaxIter': hyp_maxiterations, 'bal_acc': bal_acc}
+                    # result_df = result_df.append(valueDict, ignore_index=True)
+                    # pd.concat(valueDict, result_df, ignore_index=True, sort=False)
+                    result_df.loc[len(result_df.index)] = [hyp_actfunc, hidden_layers_architecture_tupel, hyp_solver, hyp_maxiterations, bal_acc]
 
-                        bal_acc = balanced_accuracy_score(y_test, y_pred)
-
-                        # print(f'scaler: {hyp_scaler}')
-                        # print(f'activation function: {hyp_actfunc}')
-                        # print(f'architecture: {hidden_layers_architecture_tupel}')
-                        # print(f'solver: {hyp_solver}')
-                        # print(f'max iterations: {hyp_maxiterations}')
-                        # print(f'bal_acc: {bal_acc}')
-                        # print()
-
-                        valueDict = {'Scaler': hyp_scaler, 'ActFunc': hyp_actfunc, 'HiddenArch': hidden_layers_architecture_tupel, 'Solver': hyp_solver, 'MaxIter': hyp_maxiterations, 'bal_acc': bal_acc}
-                        result_df = result_df.append(valueDict, ignore_index=True)
+                    valueDict = {'ActFunc': hyp_actfunc, 'HiddenArch': hidden_layers_architecture_tupel, 'Solver': hyp_solver, 'MaxIter': hyp_maxiterations, 'bal_acc': bal_acc}
+                    print(valueDict)
 
 result_df.to_csv(resultspath)
